0d90a43dd0ebc8cf23112e4011cb8d3775cb1a6e
Duy Khanh <khanhnd157@gmail.com>
2022-03-01 01:23:11 +0700

    Added page view

---
+++ src/CodeMaze.ViewModels/System/PageView.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeMaze.ViewModels
+{
+    public class PageView<T> where T : class
+    {
+        public T Data { get; set; }
+        public bool EnableSidebar { get; set; } = true;
+        public bool EnableComment { get; set; } = true;
+
+        public PageView()
+        {
+        }
+
+        public PageView(T data)
+        {
+            this.Data = data ?? default(T);
+        }
+    }
+}
